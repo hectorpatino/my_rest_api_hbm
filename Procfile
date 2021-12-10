72385612e425625ebb3e9b8b50d38387e8dfe9b5
Hector Patiño <hectorpatino24@gmail.com>
2021-12-10 16:33:38 -0500

    remove stopwords

--- Procfile
+++ Procfile
@@ -2,3 +2,4 @@ web: gunicorn humbolt.wsgi --log-file -
 release: python manage.py makemigrations --noinput
 release: python manage.py collectstatic --noinput
 release: python manage.py migrate --noinput
+release: python -m spacy download en
--- bored_and_joke/views.py
+++ bored_and_joke/views.py
@@ -10,9 +10,14 @@ from rest_framework.views import APIView
 from rest_framework.settings import api_settings
 from rest_framework_csv import renderers as r
 import spacy
+from spacy.tokenizer import Tokenizer
+from spacy.lang.en import English
+import random
 
+nlp = English()
 sp = spacy.load('en_core_web_sm')
-all_stopwords = sp.Defaults.stop_words
+stopwords = sp.Defaults.stop_words
+
 
 @api_view(['GET'])
 def get_bored_and_joke(request, tipo):
@@ -29,24 +34,33 @@ def get_bored_and_joke(request, tipo):
                 'actividad': bored_api_data['activity'],
                 'key': bored_api_data['key']
             }
-            joke_api_response = requests.get(f'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw&contains={tipo}')
-            if joke_api_response.status_code == 200:
-                joke_api_data = joke_api_response.json()
-                if joke_api_data['error'] is True:
-                    # TODO mejorar este error, es decir que sea uno apropiado
-                    raise serializers.ValidationError({"error": "No se encontró una chiste"})
-                if joke_api_data['type'] == 'twopart':
-                    joke = joke_api_data['setup'] + '->' + joke_api_data['delivery']
-                else:
-                    joke = joke_api_data['joke']
-                bored_and_joke['chiste'] = joke
-                serializer = BoredAndJokeForGetSerializer(data=bored_and_joke)
-                if serializer.is_valid():
-                    serializer.save()
-                    return Response(serializer.data, status=status.HTTP_201_CREATED)
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            return Response({"error": "Error al obtener el joke"}, status=status.HTTP_400_BAD_REQUEST)
-        return Response({"error": "Error al obtener el juego"}, status=status.HTTP_400_BAD_REQUEST)
+            search_word = [word.lower() for word in bored_api_data['activity'].split() if word.lower() not in stopwords]
+            if len(search_word) >= 2:
+                random_word = random.choice(search_word)
+                search_word.remove(random_word)
+                joke_api_response = requests.get(f'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw&contains={random_word}')
+                if joke_api_response.status_code == 200:
+                    joke_api_data = joke_api_response.json()
+                    if joke_api_data['error'] is True:
+                        random_word = random.choice(search_word)
+                        joke_api_response = requests.get(
+                            f'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw&contains={random_word}')
+                        if joke_api_response.status_code == 200:
+                            joke_api_data = joke_api_response.json()
+                            if joke_api_data['error'] is True:
+                                raise serializers.ValidationError({"error": "No se encontró una chiste"})
+                    if joke_api_data['type'] == 'twopart':
+                        joke = joke_api_data['setup'] + '->' + joke_api_data['delivery']
+                    else:
+                        joke = joke_api_data['joke']
+                    bored_and_joke['chiste'] = joke
+                    serializer = BoredAndJokeForGetSerializer(data=bored_and_joke)
+                    if serializer.is_valid():
+                        serializer.save()
+                        return Response(serializer.data, status=status.HTTP_201_CREATED)
+                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"error": "Error al obtener el joke"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "Error al obtener el juego"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
